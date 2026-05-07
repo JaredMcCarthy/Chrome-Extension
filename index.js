6e5adbfa254fdfb9103741450c3c80c563617b1c
jared <jared.lopezmccarthy05@gmail.com>
2026-05-07 13:33:31 -0600

    features de ayer del boton con eventlistener

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+let myLeads = []
+const inputEl = document.getElementById("input-el"); //llamamos al input de hmtl
+
+// llamando al boton de HTML
+const inputBtn = document.getElementById("input-btn");
+
+
+//funcion de llamar al presionar
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
+
+
+
+
