f8dcf5e8d1c73a392e895665367fbcbac574d1b5
jared <jared.lopezmccarthy05@gmail.com>
2026-05-11 19:24:33 -0600

    features nuevas con localstorage y funciones extras

--- index.js
+++ index.js
@@ -6,20 +6,33 @@ const inputBtn = document.getElementById("input-btn"); // llamando al boton de H
 const ulEl = document.getElementById("ul-el ");
 
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads")); //tomamos los datos de myleads dentro de localstorage
+
+//if revisa si leads.. es tru o falsy para guardar en localstorage y correr la otra funcion
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads()
+}
+
+
 //funcion de llamar al presionar
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value); // con el .value tenermos el dato del input
-  console.log(myLeads);
-  inputEl.value = "";  //limpia el input al dar click
-  renderLeads() //llamamos la funcion al hacer click
+    myLeads.push(inputEl.value); // con el .value tenermos el dato del input
+    // inputEl.value = "";  //limpia el input al dar click
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) //al hacer click los guarda los array en el local storage
+
+    renderLeads() //llamamos la funcion al hacer click
+    console.log(localStorage.getItem("myLeads"));
 });
+
+
 
 
 // la funcion sirve para mover los elementos en el input a una lista myLeads
 // tambien el For pone el order de los elementos guardados con er innerHTML
 
 function renderLeads() {
-    let listItems = []
+    let listItems = ""
     for (i = 0; i < myLeads.length; i++) {
         //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
@@ -30,6 +43,6 @@ function renderLeads() {
             </li>
         `
     } // esto es lo mismo que la linea que esta // pero como template (muy complicado) (practicar)
-    }
     ulEl.innerHTML = listItems
 }
+
