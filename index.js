a4a722f85d3a25662fef795f4eba32dc40f85e06
jared <jared.lopezmccarthy05@gmail.com>
2026-05-29 16:06:57 -0600

    feature final del nuevo boton y su funcion, ademas se areglo la lista global para guardar los json en una lista fija y no en local, lanzamiento final

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
-
-
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el"); //llamamos al input de hmtl
 const inputBtn = document.getElementById("input-btn"); // llamando al boton de HTML
 const ulEl = document.getElementById("ul-el ");
-
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads")); //tomamos los datos de myleads dentro de localstorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads")); //tomamos los datos de myleads dentro de localstorage
+const deletenBtn = document.getElementById("delete-btn");
+const tabbtn = document.getElementById("tab-btn");
 
 //if revisa si leads.. es tru o falsy para guardar en localstorage y correr la otra funcion
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    renderLeads()
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function () {
+    console.log(tabs);
+
+    //este de aqui abajo es para guardar al momento de press el boton
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (i = 0; i < leads.length; i++) {
+    //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+  } // esto es lo mismo que la linea que esta // pero como template (muy complicado) (practicar)
+  ulEl.innerHTML = listItems;
+}
+
+//boton que aceta solo doble clicks con dblclick - es nueva esta
+deletenBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+}); //VACIA LA LISTA AL HACER DOBLE CLICK AL BOTON DE DELETE
 
 //funcion de llamar al presionar
 inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value); // con el .value tenermos el dato del input
-    // inputEl.value = "";  //limpia el input al dar click
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) //al hacer click los guarda los array en el local storage
+  myLeads.push(inputEl.value); // con el .value tenermos el dato del input
+  inputEl.value = ""; //limpia el input al dar click
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); //al hacer click los guarda los array en el local storage
 
-    renderLeads() //llamamos la funcion al hacer click
-    console.log(localStorage.getItem("myLeads"));
+  render(myLeads); //llamamos la funcion al hacer click
+  console.log(localStorage.getItem("myLeads"));
 });
-
-
-
-
-// la funcion sirve para mover los elementos en el input a una lista myLeads
-// tambien el For pone el order de los elementos guardados con er innerHTML
-
-function renderLeads() {
-    let listItems = ""
-    for (i = 0; i < myLeads.length; i++) {
-        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    } // esto es lo mismo que la linea que esta // pero como template (muy complicado) (practicar)
-    ulEl.innerHTML = listItems
-}
-
